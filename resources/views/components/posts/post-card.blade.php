d0498095365c015098d85c256fbf094bdb8d44d9
Fazle Bari <fazlebarisn@gmail.com>
2023-11-21 19:18:21 +0600

    working

---
+++ resources/views/components/posts/post-card.blade.php
@@ -0,0 +1,14 @@
+@props(['post'])
+<div>
+    <div>
+        <img class="w-full rounded-xl"
+            src="{{$post->image}}">
+    </div>
+    <div class="mt-3">
+        <div class="flex items-center mb-2">
+            <p class="text-gray-500 text-sm">2023-08-29</p>
+        </div>
+        <a class="text-xl font-bold text-gray-900">Mary Berge</a>
+    </div>
+</div>
+
